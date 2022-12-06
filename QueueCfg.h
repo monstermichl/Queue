e7c57e8c5306cfcd1522354e5ea09d858dec9389
monstermichl <vouillarmet.michel@gmail.com>
2022-12-06 08:48:21 +0100

    Update QueueCfg.h
    Add thread safety options

--- QueueCfg.h
+++ QueueCfg.h
@@ -3,13 +3,15 @@
  * @author Michel Vouillarmet
  * @brief Queue module configuration file.
  * @version 1.0
- * @date 2022-09-06
+ * @date 2022-12-06
  * @see https://github.com/monstermichl/Queue
  */
 
-#ifndef C_QUEUE_CFG_HEADER
-#define C_QUEUE_CFG_HEADER
+#define QUEUE_ONE_SIZE_TO_RULE_THEM_ALL (0u) /* If this is set to anything but 0, the size for each element can only be set when the queue instance is initialized. */
+#define QUEUE_THREAD_SAFE               (0u) /* If this is set to anything but 0, the queues use mutexes to be thread safe. */
 
-#define ONE_SIZE_TO_RULE_THEM_ALL (0u) /* If this is set to anything but 0, the size for each element can only be set when the queue instance is initialized. */
-
+/* WARNING: The following flags should only be used for debugging purpose as they can dramatically decrease performance! */
+#if QUEUE_THREAD_SAFE
+#define QUEUE_LOCK_CALLBACK             (0u) /* If this is set to anything but 0, a callback will be triggered for each queue lock. */
+#define QUEUE_UNLOCK_CALLBACK           (0u) /* If this is set to anything but 0, a callback will be triggered for each queue unlock. */
 #endif
